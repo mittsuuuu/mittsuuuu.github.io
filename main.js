b4b67ab2f586195141b60f1ace2488911bddc5b9
[mittsuuuu] <[MiTsuru1024_blue..]>
2025-06-30 15:05:57 +0900

    色々と変更 基本的なアニメーションは作成できたので内容を充実させていく

--- main.js
+++ main.js
@@ -3,10 +3,43 @@ const { createApp } = Vue
 createApp({
   data() {
     return {
-      message: 'Hello Vue!'
+      displayedMessage: '',
+      tocItems: [],
+      isLeftColumnVisible: true
+    }
+  },
+  mounted() {
+    const message = '佐藤光流 ポートフォリオ';
+    let i = 0;
+    const typing = setInterval(() => {
+      if (i < message.length) {
+        this.displayedMessage += message.charAt(i);
+        i++;
+      } else {
+        clearInterval(typing);
+      }
+    }, 150);
+
+    this.generateToc();
+  },
+  methods: {
+    generateToc() {
+      const content = document.getElementById('content');
+      const headings = content.querySelectorAll('h2');
+      headings.forEach((heading, index) => {
+        const id = 'section-' + index;
+        heading.id = id;
+        this.tocItems.push({ id: id, text: heading.textContent });
+      });
+    },
+    scrollTo(id) {
+      const element = document.getElementById(id);
+      if (element) {
+        element.scrollIntoView({ behavior: 'smooth' });
+      }
+    },
+    toggleLeftColumn() {
+      this.isLeftColumnVisible = !this.isLeftColumnVisible;
     }
   }
-}).mount('#app')
-
-
-//=====//
+}).mount('#app');
